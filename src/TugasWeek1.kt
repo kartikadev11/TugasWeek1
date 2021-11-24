4a49538fbcdb4bfafe3851bdd7bf6b5184a093be
kartikadev11 <kartikadevianii@gmail.com>
2021-11-24 16:44:45 +0700

    Initial commit

---
+++ src/TugasWeek1.kt
@@ -0,0 +1,9 @@
+fun main () {
+    var biodata : String ="""
+        Nama  : Kartika Deviani
+        NIM   : 21090097
+        Kelas : 1C
+        Prodi : D4 Teknik Informatika
+    """.trimIndent()
+    println(biodata)
+}
